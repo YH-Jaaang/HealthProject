25a1b9dd760748a76e0e1d3386e16b670035d028
Jang <jangyh1995@gmail.com>
2021-08-18 21:46:54 +0900

    5

--- src/main/java/com/mini/health/controller/MainController.java
+++ src/main/java/com/mini/health/controller/MainController.java
@@ -53,7 +53,6 @@ public class MainController {
     }
     @PostMapping("/login")
 	public String login(@RequestParam("id") String id, @RequestParam("pass") String pass, RedirectAttributes redirect,HttpSession session) {
-        System.out.println("dds");
         return loginService.login(id,pass,redirect,session);
 	}
 }
